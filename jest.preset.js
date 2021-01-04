b79b387dde2bf0ee5c0e7d05ac703039239c7676
hank <hankchiu.tw@gmail.com>
2021-01-04 21:53:30 +0800

    test: enable coverage summary

--- jest.preset.js
+++ jest.preset.js
@@ -3,4 +3,6 @@ const nxPreset = require('@nrwl/jest/preset');
 module.exports = {
   ...nxPreset,
   setupFiles: ['jest-canvas-mock', 'jest-webextension-mock'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'text-summary', 'html-spa'],
 };
